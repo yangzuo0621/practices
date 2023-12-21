6c6a5b0e8203aa18536dae8bfc88a9774f1cad6a
Zuo Yang <zuya@microsoft.com>
2023-12-21 14:51:18 +0800

    Problem 3: Largest Prime Factor

--- euler/src/euler/core.clj
+++ euler/src/euler/core.clj
@@ -1,6 +1,23 @@
 (ns euler.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; https://projecteuler.net/problem=3
+;; Largest Prime Factor
+
+(defn all-primes [n]
+  (let [scope (range 2 (int (Math/sqrt n)))
+        coll (range 2 n)]
+    (loop [s scope result coll]
+      (if (empty? s)
+        result
+        (recur (rest s) 
+               (filter #(or (not= 0 (mod % (first s))) (= % (first s))) 
+                       result))))))
+
+(defn largest-prime-factor
+  "largest prime factor of 600851475143: 6857"
+  []
+  (let [target 600851475143]
+    (first (drop-while #(not= 0 (mod target %))
+                       (reverse (all-primes (int (Math/sqrt target))))))
+    )
+)
